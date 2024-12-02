0d22d0ecbbd7e5c98c9c11fdd778567c1feea78d
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2024-12-03 00:09:04 +0100

    refactor: Switch database connection from MySQL to GORM

--- api/internal/database/database.go
+++ api/internal/database/database.go
@@ -1,20 +1,20 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/Cosmos307/todo-app/api/internal/config"
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func NewDB(cfg *config.Config) (*sql.DB, error) {
+func NewDB(cfg *config.Config) (*gorm.DB, error) {
 	// Set Data Source Name
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
 
-	var db *sql.DB
+	var db *gorm.DB
 	var err error
 	maxRetries := 5
 	retryInterval := 5 * time.Second
@@ -22,14 +22,21 @@ func NewDB(cfg *config.Config) (*sql.DB, error) {
 	// Connect to the database with retries
 	for i := 0; i < maxRetries; i++ {
 		// Connect to the database
-		if db, err = sql.Open("mysql", dsn); err != nil {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
 			log.Printf("Error connecting to the database: %v", err)
 			time.Sleep(5 * retryInterval)
 			continue
 		}
 
-		// Test connection
-		if pingError := db.Ping(); pingError != nil {
+		// Extract the underlying *sql.DB instance to ping test and ensure a valid connection
+		sqlDB, err := db.DB()
+		if err != nil {
+			log.Printf("Error getting database instance: %v", err)
+			time.Sleep(retryInterval)
+			continue
+		}
+		if pingError := sqlDB.Ping(); pingError != nil {
 			log.Printf("Error pinging the database: %v", pingError)
 			time.Sleep(5 * retryInterval)
 			continue
