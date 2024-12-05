f7f4181dd199c853d23198b4ab0639dadb34fb29
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2024-12-05 20:48:11 +0100

    Add MySQL-specific repository struct with methods for MySQL database

---
+++ api/internal/repository/user_repository_mysql.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"github.com/Cosmos307/todo-app/api/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepositoryMySQL struct {
+	db *gorm.DB
+}
+
+func NewUserRepositoryMySQL(db *gorm.DB) UserRepository {
+	return &UserRepositoryMySQL{db: db}
+}
+
+func (r *UserRepositoryMySQL) GetUserByID(userID int) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, userID).Error
+	return &user, err
+}
+
+func (r *UserRepositoryMySQL) CreateUser(user *models.User) error {
+	err := r.db.Create(user).Error
+	return err
+}
+
+func (r *UserRepositoryMySQL) UpdateUser(user *models.User) error {
+	err := r.db.Save(user).Error
+	return err
+}
+
+func (r *UserRepositoryMySQL) DeleteUserByID(userID int) error {
+	err := r.db.Delete(&models.User{}, userID).Error
+	return err
+}
