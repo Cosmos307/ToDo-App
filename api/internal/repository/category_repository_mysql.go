068e0ee349bc2e765594ecbb9f300b4a4c1e6ccc
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2024-12-05 21:20:51 +0100

    Add MySQL-specific repository struct with methods for MySQL database

---
+++ api/internal/repository/category_repository_mysql.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"github.com/Cosmos307/todo-app/api/internal/models"
+	"gorm.io/gorm"
+)
+
+type CategoryRepositoryMySQL struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepositoryMySQL(db *gorm.DB) CategoryRepository {
+	return &CategoryRepositoryMySQL{db: db}
+}
+
+func (r *CategoryRepositoryMySQL) GetCategoryByID(categoryID int) (*models.Category, error) {
+	var category *models.Category
+	err := r.db.First(category, categoryID).Error
+	return category, err
+}
+
+func (r *CategoryRepositoryMySQL) CreateCategory(category *models.Category) error {
+	err := r.db.Create(category).Error
+	return err
+}
+
+func (r *CategoryRepositoryMySQL) UpdateCategory(category *models.Category) error {
+	err := r.db.Save(category).Error
+	return err
+}
+
+func (r *CategoryRepositoryMySQL) DeleteCategoryByID(categoryID int) error {
+	err := r.db.Delete(&models.Category{}, categoryID).Error
+	return err
+}
