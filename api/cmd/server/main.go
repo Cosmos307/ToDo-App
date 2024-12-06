3df1102d55f9092d3fa7c64afe5f14beb66cb0af
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2024-12-06 13:31:59 +0100

    Refactor: Update main.go to use routes registration

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -8,6 +8,9 @@ import (
 
 	"github.com/Cosmos307/todo-app/api/internal/config"
 	"github.com/Cosmos307/todo-app/api/internal/database"
+	"github.com/Cosmos307/todo-app/api/internal/handlers"
+	"github.com/Cosmos307/todo-app/api/internal/repository"
+	"github.com/Cosmos307/todo-app/api/internal/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,9 +25,14 @@ func main() {
 		log.Fatal("Database connection is nil")
 	}
 
+	categoryRepo := repository.NewCategoryRepositoryMySQL(database)
+	categoryHandler := handlers.NewCategoryHandler(categoryRepo)
+
 	router := gin.Default()
+
+	routes.RegisterRoutes(router, categoryHandler)
 
 	port := os.Getenv("API_PORT")
 	fmt.Printf("Server running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+	log.Println(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
