539996d9499cb085e884624a3575a17b290041e9
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2024-12-08 03:10:04 +0100

    Feat: Add initialisation of repository, handler and register routes

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -8,6 +8,9 @@ import (
 
 	"github.com/Cosmos307/todo-app/api/internal/config"
 	"github.com/Cosmos307/todo-app/api/internal/database"
+	"github.com/Cosmos307/todo-app/api/internal/handlers"
+	"github.com/Cosmos307/todo-app/api/internal/repository"
+	"github.com/Cosmos307/todo-app/api/internal/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,6 +27,15 @@ func main() {
 
 	router := gin.Default()
 
+	userRepository := repository.NewUserRepositoryMySQL(database)
+	taskRepository := repository.NewTaskRepositoryMySQL(database)
+	categoryRepository := repository.NewCategoryRepositoryMySQL(database)
+
+	userHandler := handlers.NewUserHandler(userRepository)
+	taskHandler := handlers.NewTaskHandler(taskRepository)
+	categoryHandler := handlers.NewCategoryHandler(categoryRepository)
+
+	routes.RegisterRoutes(router, userHandler, taskHandler, categoryHandler)
 	port := os.Getenv("API_PORT")
 	fmt.Printf("Server running on port %s\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
