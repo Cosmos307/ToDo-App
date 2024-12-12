31e8e96db2d83f49a82e784bd9c083cb67979fb1
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2024-12-12 22:56:40 +0100

    Feat: Add MockCategoryRepository with CRUD methods for testing

---
+++ api/internal/repository/mocks/category_repository_mock.go
@@ -0,0 +1,47 @@
+package mocks
+
+import (
+	"errors"
+
+	"github.com/Cosmos307/todo-app/api/internal/models"
+	"github.com/Cosmos307/todo-app/api/internal/repository"
+)
+
+type MockCategoryRepository struct {
+	categories map[int]*models.Category
+}
+
+func NewMockCategoryRepository() repository.CategoryRepository {
+	return &MockCategoryRepository{
+		categories: make(map[int]*models.Category),
+	}
+}
+
+func (m *MockCategoryRepository) GetCategoryByID(categoryID int) (*models.Category, error) {
+	if category, exists := m.categories[categoryID]; exists {
+		return category, nil
+	}
+	return nil, errors.New("category not found")
+}
+
+func (m *MockCategoryRepository) CreateCategory(category *models.Category) (*models.Category, error) {
+	category.ID = len(m.categories)
+	m.categories[category.ID] = category
+	return m.categories[category.ID], nil
+}
+
+func (m *MockCategoryRepository) UpdateCategoryByID(category *models.Category) error {
+	if _, exists := m.categories[category.ID]; exists {
+		m.categories[category.ID] = category
+		return nil
+	}
+	return errors.New("category not found")
+}
+
+func (m *MockCategoryRepository) DeleteCategoryByID(categoryID int) error {
+	if _, exists := m.categories[categoryID]; exists {
+		delete(m.categories, categoryID)
+		return nil
+	}
+	return errors.New("category not found")
+}
